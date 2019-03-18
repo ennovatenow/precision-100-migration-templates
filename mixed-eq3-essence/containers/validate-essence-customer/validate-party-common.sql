3398917ca40c4977394ed649fb43a829cb6369d1
ennovatenow <anish.thomas@evolvussolutions.com>
2019-03-18 14:58:46 +0530

    added validation and file load

--- mixed-eq3-essence/containers/validate-essence-customer/validate-party-common.sql
+++ mixed-eq3-essence/containers/validate-essence-customer/validate-party-common.sql
@@ -1,5 +1,18 @@
+/*
+** Here we collect all the records which do not match the default values?
+** Simple validation list 
+** v_party_common
+** Length(column) > max_length
+** column_value <> default value
+** mandatory column is null or space
+** colum value <> list
+**
+*/ 
 CREATE OR REPLACE VIEW V_PARTY_COMMON AS 
-SELECT 'Invalid Party Type', COUNT(1) FROM O_PARTY_COMMON WHERE (PARTYTYPE <> '1062' AND PARTYTYPE <> '1063')
+-- PartyType has validation of type list
+SELECT 'Invalid Party Type', COUNT(1) 
+FROM   O_PARTY_COMMON 
+WHERE  PartyType NOT IN ( SELECT LOOKUP_VALUE FROM S_LOOKUP WHERE LOOKUP_TYPE = 'O_PARTY_COMMON.PARTYTYPE' )
 UNION
 --SELECT 'Invalid Party Sub Type', COUNT(1) FROM O_PARTY_COMMON WHERE (PARTYSUBTYPE <> '1062' AND PARTYSUBTYPE <> '1063')
 --UNION
