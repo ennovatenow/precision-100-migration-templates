8677a44c420a8b42c885d1685e15dbd81117e7e9
rajthay <rajthay@yahoo.com>
2019-04-02 15:16:53 +0530

    Update extract-mapper.sh

--- mixed-eq3-essence/containers/load-mappers/extract-mapper.sh
+++ mixed-eq3-essence/containers/load-mappers/extract-mapper.sh
@@ -4,3 +4,5 @@ mkdir -p $SQLLDR_INPUT
 echo "Extracting table map_codes"
 
 cp $CONTAINER_FOLDER/$CONTAINER/map_codes.dat $SQLLDR_INPUT
+cp $CONTAINER_FOLDER/$CONTAINER/map_product.dat $SQLLDR_INPUT
+cp $CONTAINER_FOLDER/$CONTAINER/prod_subprod_param.dat $SQLLDR_INPUT
