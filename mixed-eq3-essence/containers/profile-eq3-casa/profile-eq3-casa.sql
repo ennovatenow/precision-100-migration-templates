6be9b21460ef027b8dbec5b3dab4c85bef57a323
rajthay <rajthay@yahoo.com>
2019-04-04 14:59:50 +0530

    Add files via upload

---
+++ mixed-eq3-essence/containers/profile-eq3-casa/profile-eq3-casa.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE VIEW PR_EQ3_VOLUME ("TABLE_NAME", "RECORD_COUNT") AS
+select 'Number of CASA accounts in Legacy', count(*) from scpf
+union all
+select 'Number of ZERO balance accounts', count(*) from scpf where scbal  = 0
+union all
+select 'Number of NON-ZERO balance accounts', count(*) from scpf where scbal <> 0
+union all
+select 'Number of Accounts with Limits', count(*) from scpf where scodl <> 0
+union all
+select 'Number of dormant accounts', count(*) from scpf where scai20 ='Y'
+union all
+select 'Number of blocked accounts', count(*) from scpf where scai17 ='Y'
+union all
+select 'Number of Closed accounts', count(*) from scpf where scai30 ='Y'
+union all
+select 'Number of accounts with Lien', count(*) from scpf where scrba <>0
