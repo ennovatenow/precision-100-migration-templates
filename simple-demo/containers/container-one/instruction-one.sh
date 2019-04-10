d8c54a15a803e91d6da22862ad77e6b6fa3e41df
ennovatenow <anish.thomas@evolvussolutions.com>
2019-04-10 12:00:52 +0530

    rejigged simple-demo

---
+++ simple-demo/containers/container-one/instruction-one.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Got to instruction: $0"
+
+env | grep PRECISION 
---
+++ simple-demo/containers/container-one/instruction-two.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Got to instruction: $0"
+
+echo "We have seen the env ..and it is good..."
